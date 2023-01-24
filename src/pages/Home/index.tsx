a59113974a160b8d23ed0373e2fa2106a654377a
zhangjing <zhangjing@qianjiangcloud.com>
2023-01-24 21:21:07 +0800

    fix: types

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,12 @@
-import { useEffect } from 'react'
-import type { LegacyRef } from 'react'
 import { Outlet } from 'react-router-dom'
 import { TopBar } from '@/components'
 import { useTopBar } from '@/hooks'
+import { MouseEventHandler } from 'react'
 export default function Home() {
-  const { nav, indicatorBar, onNavItemClick, dropDownItems, goHome } = useTopBar()
+  const { nav, onNavItemClick, dropDownItems, goHome } = useTopBar()
   return (
     <>
-      <TopBar goHome={goHome} dropDownItems={dropDownItems} nav={nav} indicatorBar={indicatorBar as LegacyRef<HTMLSpanElement>} onNavItemClick={onNavItemClick} />
+      <TopBar goHome={goHome} dropDownItems={dropDownItems} nav={nav} onNavItemClick={onNavItemClick as any as MouseEventHandler} />
       <Outlet />
     </>
   )
